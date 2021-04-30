dcbf63a40b2da0e357673f203c06a39514b6d019
Eric Le Donge <eledonge@hortau.com>
2021-04-30 15:25:36 -0400

    First Fetching and FlatList

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
 export default function App() {
+  const [isLoading, setLoading] = React.useState(true);
+  const [todos, setTodos] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => response.json())
+      .then((json) => setTodos(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, [])
+
+  const renderItems = (({item}) => (<View style={{paddingVertical: 10}}><Text>{item.title}</Text></View>))
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> :
+        (<FlatList
+          data={todos}
+          keyExtractor={({ id }) => id}
+          renderItem={renderItems}
+        />)
+      }
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
